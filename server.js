be2b0439cde3ec0cb97fb807cd7fcd4f230c8f2f
bobbybeagle <92606184+bobbybeagle@users.noreply.github.com>
2021-10-16 01:40:35 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,6 @@
 const express = require('express');
 const {Client} = require('pg');
 
-console.log("testing")
-
 //Create the conection to the postgres server
 const client = new Client({
     connectionString: "postgres://preulkdjfcooip:2c96e22554eca8177be0eda4b2c968d4def7e0e8f882d832cdeb6e57d1e2741c@ec2-52-87-123-108.compute-1.amazonaws.com:5432/d41t5afauefh19"
@@ -23,16 +21,20 @@ app.post('/query', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     console.log("Receiving request");
     if(req.body.query) {
+        console.log("phase1")
         console.log(req.body.query);
         client.query(req.body.query, (err, r) => {
+             console.log("phase2")
             if (err) throw err;
             rows = [];
             for(let row of r.rows){
                 rows.push(row);
             }
+             console.log("phase3")
             response = JSON.stringify(rows);
             console.log(response);
             res.end(response);
+             console.log("phase4")
         });
     }
 });
